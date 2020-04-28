586bbad498083113a0bd3224b978897650e734bc
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-04-28 09:16:50 -0400

    first commit

---
+++ ClientSideValidation_Lab16.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientSideValidation_Lab16._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
